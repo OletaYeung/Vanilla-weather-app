76e7e37fe701071a34d32a41b0b92523d4cac2f5
Oleta <oletayeung@gmail.com>
2022-05-22 11:55:40 +1000

    Updated the date in JS

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+function formateDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  dateElement.innerHTML = formateDate(response.data.dt * 1000);
 }
 
 let apikey = "8f7be36fb8a4700a809030b5729bb13a";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${apikey}&units=metric`;
-console.log(apiUrl);
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+
 axios.get(apiUrl).then(displayTemperature);
